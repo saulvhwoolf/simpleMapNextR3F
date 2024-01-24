71a76427e29b5044e6e84e6db5c830d9ec77943b
saulvhwoolf <saulvhwoolf@gmail.com>
2024-01-24 00:37:30 -0500

    switched to google storage instead of local

--- next.config.js
+++ next.config.js
@@ -16,6 +16,20 @@ const nextConfig = {
             },
         ],
     },
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "false" },
+                    { key: "Access-Control-Allow-Origin", value: "https://storage.googleapis.com" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date" },
+                ]
+            }
+        ]
+    }
 }
 
 module.exports = nextConfig
