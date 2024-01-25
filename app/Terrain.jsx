88fea471dcc3dd03d178bdb2ba7dc1d507da42e6
saulvhwoolf <saulvhwoolf@gmail.com>
2024-01-25 18:53:15 -0500

    dynamic height

--- app/Terrain.jsx
+++ app/Terrain.jsx
@@ -7,8 +7,23 @@ import {Plane} from "@react-three/drei";
 export const Terrain = ({showWireFrame, heightMapUrl, textureUrl, heightRange, dimensions}) => {
     const height = useLoader(THREE.TextureLoader, heightMapUrl);
     let color = useLoader(THREE.TextureLoader, textureUrl);
-    const dimRatio = getDimensionRatio(dimensions)
-    const veriticalScale = (heightRange[1]-heightRange[0])/512
+
+
+    const MAX_WIDTH_OR_HEIGHT = 64
+    // m * largerdimension = 64
+    const largerDimension = Math.max(dimensions[0], dimensions[1])
+    const smallerDimension = Math.min(dimensions[0], dimensions[1])
+    const multiplier = 64 / largerDimension
+
+    const dimRatio = [multiplier * dimensions[0], multiplier * dimensions[1]]
+
+    // const n = (smallerDimension / largerDimension) * MAX_WIDTH_OR_HEIGHT
+    // const dimRatio = (dimensions[0] > dimensions[1]? [MAX_WIDTH_OR_HEIGHT, n] : [n, MAX_WIDTH_OR_HEIGHT])
+
+
+    const verticalScale = multiplier * (heightRange[1] - heightRange[0]) / 10
+        // const verticalScale = 5
+    console.log(verticalScale, dimRatio, heightRange[0], heightRange[1], dimensions[0], dimensions[1])
 
     return (
         <group>
@@ -22,10 +37,10 @@ export const Terrain = ({showWireFrame, heightMapUrl, textureUrl, heightRange, d
                     color="white"
                     wireframe={true}
                     displacementMap={height}
-                    displacementScale={veriticalScale}
+                    displacementScale={verticalScale}
                 />
             </Plane>
-            {showWireFrame?<></>
+            {showWireFrame ? <></>
                 :
                 <Plane
                     rotation={[-Math.PI / 2, 0, 0]}
@@ -35,11 +50,10 @@ export const Terrain = ({showWireFrame, heightMapUrl, textureUrl, heightRange, d
                     <meshStandardMaterial
                         attach="material"
                         color="white"
-                        wireframe={false}
                         map={color}
                         metalness={.2}
                         displacementMap={height}
-                        displacementScale={veriticalScale}
+                        displacementScale={verticalScale}
                     />
                 </Plane>
             }
@@ -56,7 +70,7 @@ function getDimensionRatio(dimension) {
     const smallerDimension = Math.min(dimension[0], dimension[1])
     const n = (smallerDimension / largerDimension) * MAX_WIDTH_OR_HEIGHT
 
-    return (dimension[0] > dimension[1]? [MAX_WIDTH_OR_HEIGHT, n] : [n, MAX_WIDTH_OR_HEIGHT])
+    return (dimension[0] > dimension[1] ? [MAX_WIDTH_OR_HEIGHT, n] : [n, MAX_WIDTH_OR_HEIGHT])
     // height / width  = n / 64
     // width  / height = n / 64
     // const ratio = heightRange[0]/heightRange[1]
